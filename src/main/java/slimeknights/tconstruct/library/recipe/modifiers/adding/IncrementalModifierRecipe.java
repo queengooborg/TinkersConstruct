c4cb3a107e49d7ef0e79c8ab193a3e0298bad3fc
KnightMiner <knightminer4@gmail.com>
2024-04-06 19:56:07 -0400

    Fix incremental modifier recipes returning wrong leftovers
    Was inverted, so with say, redstone instead of getting 2 you would get 7
    
    (cherry picked from commit 4cce21f022ab039132a29854d00abb3a881d9f68)

--- src/main/java/slimeknights/tconstruct/library/recipe/modifiers/adding/IncrementalModifierRecipe.java
+++ src/main/java/slimeknights/tconstruct/library/recipe/modifiers/adding/IncrementalModifierRecipe.java
@@ -1,6 +1,7 @@
 package slimeknights.tconstruct.library.recipe.modifiers.adding;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.math.IntMath;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 import net.minecraft.network.FriendlyByteBuf;
@@ -278,7 +279,9 @@ public class IncrementalModifierRecipe extends AbstractModifierRecipe {
     if (leftoverAmount > 0) {
       itemsNeeded++;
       if (!leftover.isEmpty()) {
-        inv.giveItem(ItemHandlerHelper.copyStackWithSize(leftover, leftoverAmount * leftover.getCount()));
+        // leftoverAmount refers to how many we need to that is does not fit cleanly into amountPerInput
+        // but we want to return the amount we did not use, hence the subtraction
+        inv.giveItem(ItemHandlerHelper.copyStackWithSize(leftover, (amountPerInput - leftoverAmount) * leftover.getCount()));
       }
     }
     for (int i = 0; i < inv.getInputCount(); i++) {
