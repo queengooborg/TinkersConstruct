122d3e74e650e9e9b6c4b0800f98bbd143fb28eb
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-10-20 01:36:09 -0700

    Fix ConfiguredFeature

--- src/main/java/slimeknights/tconstruct/world/TinkerWorld.java
+++ src/main/java/slimeknights/tconstruct/world/TinkerWorld.java
@@ -10,6 +10,7 @@ import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.entity.EntityType;
@@ -211,8 +212,8 @@ public final class TinkerWorld extends TinkerModule {
     Function<FoliageType,BlockBehaviour.Properties> props = type -> builder(Material.PLANT, type.getMapColor(), type.isNether() ? SoundType.FUNGUS : SoundType.GRASS).instabreak().noCollission();
     return new EnumObject.Builder<FoliageType,Block>(FoliageType.class)
       .putAll(BLOCKS.registerEnum(FoliageType.OVERWORLD, "slime_sapling", (type) -> new SlimeSaplingBlock(new SlimeTree(type), type, props.apply(type).randomTicks()), (b) -> new BlockTooltipItem(b, new Item.Properties())))
-      .put(FoliageType.BLOOD, BLOCKS.register("blood_slime_sapling", () -> new SlimeFungusBlock(props.apply(FoliageType.BLOOD), () -> TinkerStructures.bloodSlimeFungus.getHolder().orElseThrow()), (b) -> new BlockTooltipItem(b, new Item.Properties())))
-      .put(FoliageType.ICHOR, BLOCKS.register("ichor_slime_sapling", () -> new SlimeFungusBlock(props.apply(FoliageType.ICHOR), () -> TinkerStructures.ichorSlimeFungus.getHolder().orElseThrow()), (b) -> new BlockItem(b, new Item.Properties())))
+      .put(FoliageType.BLOOD, BLOCKS.register("blood_slime_sapling", () -> new SlimeFungusBlock(props.apply(FoliageType.BLOOD), TinkerStructures.bloodSlimeFungus.getKey()), (b) -> new BlockTooltipItem(b, new Item.Properties())))
+      .put(FoliageType.ICHOR, BLOCKS.register("ichor_slime_sapling", () -> new SlimeFungusBlock(props.apply(FoliageType.ICHOR), TinkerStructures.ichorSlimeFungus.getKey()), (b) -> new BlockItem(b, new Item.Properties())))
       .put(FoliageType.ENDER, BLOCKS.register("ender_slime_sapling", () -> new SlimePropaguleBlock(new SlimeTree(FoliageType.ENDER), FoliageType.ENDER, props.apply(FoliageType.ENDER)), (b) -> new BlockTooltipItem(b, new Item.Properties())))
       .build();
   });
--- src/main/java/slimeknights/tconstruct/world/block/SlimeFungusBlock.java
+++ src/main/java/slimeknights/tconstruct/world/block/SlimeFungusBlock.java
@@ -2,8 +2,10 @@ package slimeknights.tconstruct.world.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FungusBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
@@ -14,8 +16,8 @@ import java.util.function.Supplier;
 
 /** Update of fungus that grows on slime soil instead */
 public class SlimeFungusBlock extends FungusBlock {
-  public SlimeFungusBlock(Properties properties, Supplier<Holder<ConfiguredFeature<HugeFungusConfiguration,?>>> fungusFeature) {
-    super(properties, fungusFeature);
+  public SlimeFungusBlock(Properties properties, ResourceKey<ConfiguredFeature<?,?>> fungusFeature, Block requiredBlock) {
+    super(properties, (ResourceKey<ConfiguredFeature<?, ?>>) (ResourceKey<?>) fungusFeature, requiredBlock);
   }
 
   @Override
